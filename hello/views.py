7086430c9382efa785bb8cfd62162cae889621b0
Aman-Singh123 <amansingh701173@gmail.com>
2023-01-18 16:09:17 +0530

    v3

--- hello/views.py
+++ hello/views.py
@@ -6,14 +6,14 @@ from hello.forms import LogMessageForm
 from django.shortcuts import redirect
 from django.views.generic import ListView
 import datetime
+from django.http import JsonResponse
+import json
+from .models import LogMessage
 
+def home(request):
+    lg=LogMessage.objects.all()
+    return render(request,'hello/home.html',{"lg":lg})
 
-class HomeListView(ListView):
-    """Renders the home page, with a list of all messages."""
-    model = LogMessage
-    def get_context_data(self, **kwargs):
-        context = super(HomeListView, self).get_context_data(**kwargs)
-        return context
 
 def about(request):
     return render(request, "hello/about.html")
@@ -25,14 +25,14 @@ def contact(request):
 # make the view for the data 
 def  log_message(request):
     form = LogMessageForm(request.POST or None)
-
     if request.method == "POST":
         if form.is_valid():
+           
             message = form.save(commit=False)
             message.log_date = datetime.datetime.now()
             message.save()
-             # here we use the redirect mehtod for not use the hard coded 
-            # here we use this also 
-            return render(request,"hello/home.html")
+            return redirect("home1")
     else:
         return render(request, "hello/log_message.html", {"form": form})
+
+        
